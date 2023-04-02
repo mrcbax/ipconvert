b5641f0bd6f611781ff0389028a5d878f9847207
Chad Baxter <cbax@doslabelectronics.com>
2023-04-02 14:52:20 -0400

    added u32 to IP

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,33 @@ use std::net::Ipv4Addr;
 
 fn main() {
     match env::args().nth(1) {
-        Some(s) => {
-            let first_ip: Ipv4Addr = s.parse().expect("Invalid IPv4 address in position 1.");
-            let conversion1: u32 = first_ip.into();
-            if env::args().nth(2).is_some() {
-                let second_ip: Ipv4Addr = env::args()
-                    .nth(2)
-                    .unwrap()
-                    .parse()
-                    .expect("Invalid IPv4 address in position 2.");
-
-                let conversion2: u32 = second_ip.into();
-                for ip in conversion1..conversion2 {
-                    println!("{}", ip);
-                }
-            } else {
-                println!("{}", conversion1);
+        Some(s) => match s.trim().parse::<u32>() {
+            Ok(o) => {
+                println!("{}", Ipv4Addr::from(o));
             }
-        }
+            Err(_) => {
+                let first_ip: Ipv4Addr = s
+                    .trim()
+                    .parse()
+                    .expect("Invalid IPv4 address in position 1.");
+                let conversion1: u32 = first_ip.into();
+                if env::args().nth(2).is_some() {
+                    let second_ip: Ipv4Addr = env::args()
+                        .nth(2)
+                        .unwrap()
+                        .trim()
+                        .parse()
+                        .expect("Invalid IPv4 address in position 2.");
+
+                    let conversion2: u32 = second_ip.into();
+                    for ip in conversion1..conversion2 {
+                        println!("{}", ip);
+                    }
+                } else {
+                    println!("{}", conversion1);
+                }
+            }
+        },
         None => eprintln!("You must provide an IP address."),
     }
 }
